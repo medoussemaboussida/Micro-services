99ac47e3ca3fc8a7c1d0ece2c936cf0d349a89d5
Yasssa02 <yassinedhahbi65@esprit.tn>
2025-04-02 17:25:12 +0100

    Publication

---
+++ Publication/src/main/java/tn/esprit/publication/Controller/PublicationRestApi.java
@@ -0,0 +1,58 @@
+package tn.esprit.publication.Controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.publication.Entity.Publication;
+import tn.esprit.publication.Service.PublicationService;
+
+import java.util.List;
+@RestController
+@RequestMapping("/publication")
+public class PublicationRestApi {
+    @Autowired
+    private PublicationService publicationService;
+
+    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Publication> createPublication(@RequestBody Publication publication) {
+        return new ResponseEntity<>(publicationService.addPublication(publication), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Publication> updatePublication(@PathVariable(value = "id") int id, @RequestBody Publication publication) {
+        return new ResponseEntity<>(publicationService.updatePublication(id, publication), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deletePublication(@PathVariable(value = "id") int id) {
+        return new ResponseEntity<>(publicationService.deletePublication(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/publications")
+    public ResponseEntity<List<Publication>> getAllPublications() {
+        return new ResponseEntity<>(publicationService.getAllPublications(), HttpStatus.OK);
+    }
+
+    @GetMapping("/by-title/{title}")
+    public ResponseEntity<List<Publication>> getPublicationsByTitle(@PathVariable("title") String title) {
+        List<Publication> publications = publicationService.getByTitle(title);
+        return new ResponseEntity<>(publications, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Publication> getPublicationById(@PathVariable("id") int id) {
+        Publication publication = publicationService.getPublicationById(id);
+        if (publication != null) {
+            return new ResponseEntity<>(publication, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+}
---
+++ Publication/src/main/java/tn/esprit/publication/Repository/PublicationRepository.java
@@ -0,0 +1,19 @@
+package tn.esprit.publication.Repository;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import tn.esprit.publication.Entity.Publication;
+
+import java.util.List;
+
+public interface PublicationRepository extends JpaRepository<Publication, Integer> {
+    List<Publication> findByTitle(String title);
+    @Query("select c from Publication c where c.title like :title")
+    public Page<Publication> publicationByTitle(@Param("title") String n, Pageable pageable);
+
+
+}
