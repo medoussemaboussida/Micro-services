fddff094fd70835b8dfc3f72ddf3014f67171811
asma riahi <asma.riahi@esprit.tn>
2025-04-04 17:46:03 +0100

    Gateway

---
+++ Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,25 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+
+				.route("Appointment", r -> r.path("/appointment/**")
+						.uri("http://localhost:8056/"))
+				.build();
+	}
+}
