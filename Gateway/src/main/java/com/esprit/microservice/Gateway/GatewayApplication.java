66888482f44b5d9010deb28749bd0584e0102bd3
roumaissazd <160242132+roumaissazd@users.noreply.github.com>
2025-04-04 18:46:45 +0200

    gateway

---
+++ Gateway/src/main/java/com/esprit/microservice/Gateway/GatewayApplication.java
@@ -0,0 +1,35 @@
+package com.esprit.microservice.Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				// Route pour le microservice "candidat"
+				.route("candidat", r -> r.path("/candidats/**")
+						.uri("lb://candidat-service")) // Utilisation de Load Balancer avec Eureka
+				// Route pour le microservice "forum"
+				.route("forum", r -> r.path("/forums/**")
+						.uri("lb://forum-service")) // Utilisation de Load Balancer avec Eureka
+				// Route pour le microservice "Publication"
+				.route("publication", r -> r.path("/publications/**")
+						.uri("lb://publication-service")) // Utilisation de Load Balancer avec Eureka
+				// Route pour le microservice "Events"
+				.route("events", r -> r.path("/events/**")
+						.uri("lb://events-service")) // Utilisation de Load Balancer avec Eureka
+				.build();
+	}
+}
