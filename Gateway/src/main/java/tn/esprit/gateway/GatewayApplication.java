fbaf98954819cfa0321e1b334c2ff8d32d8760d3
Yasssa02 <yassinedhahbi65@esprit.tn>
2025-04-05 03:34:41 +0100

    Gateway

--- Gateway/src/main/java/tn/esprit/gateway/GatewayApplication.java
+++ Gateway/src/main/java/tn/esprit/gateway/GatewayApplication.java
@@ -3,6 +3,9 @@ package tn.esprit.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +13,17 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("candidat", r -> r.path("/candidats/**")
+                        .uri("http://localhost:8082/")) // Load Balancer vers le microservice "MS-CANDIDAT"
+                .route("forum", r -> r.path("/forums/**")
+                        .uri("http://localhost:8055/")) // Load Balancer vers le microservice "MS-forum"
+                .route("Publication", r -> r.path("/publications/**")
+                        .uri("http://localhost:8056/"))
+                .build();
     }
 
 }
